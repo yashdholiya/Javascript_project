86880ea7910b965a3558bc4d15377f7fd1354f82
yashdholiya <yashdholiya1108@gmail.com>
2024-03-09 16:24:42 +0530

    --

--- budget_app.js
+++ budget_app.js
@@ -1,15 +1,58 @@
-function calculateBudget() {
-    var income = parseFloat(document.getElementById("income").value);
-    var expenses = parseFloat(document.getElementById("expenses").value);
+let transactions = [];
+let totalIncome = 0;
+let totalBudget = 0;
+let totalExpense = 0;
 
-    if (isNaN(income) || isNaN(expenses)) {
-        document.getElementById("result").innerHTML = "Please enter valid numbers.";
-    } else {
-        var budget = income - expenses;
-        if (budget >= 0) {
-            document.getElementById("result").innerHTML = "You have a surplus budget of " + budget.toFixed(2);
-        } else {
-            document.getElementById("result").innerHTML = "You are in a deficit of " + Math.abs(budget).toFixed(2);
-        }
+function addBudget()
+{
+    totalBudget = parseFloat(document.getElementById('budgetsum').value);
+    updateBudgetSummary();
+}
+
+function addTransaction() {
+    const amount = parseFloat(document.getElementById("amount").value);
+    const description = document.getElementById("description").value;
+    const transactionType = document.getElementById("transactionType").value;
+
+    if (isNaN(amount) || description.trim() === "") {
+        alert("Please enter your valid amount...");
+        return;
     }
+
+    const transaction = {
+        type: transactionType,
+        amount: amount,
+        description: description
+    };
+
+    transactions.push(transaction);
+
+    if (transactionType === "income")
+    {
+        totalIncome += amount;
+    } else 
+    {
+        totalExpense += amount;
+    }
+
+    updateTransactions();
+    updateBudgetSummary();
+}
+
+function updateTransactions() 
+{
+    const transactionList = document.getElementById("transactionList");
+    transactionList.innerHTML = "";
+    transactions.forEach(transaction => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${transaction.description}: $${transaction.amount.toFixed(2)} (${transaction.type})`;
+        transactionList.appendChild(listItem);
+    });
+}
+
+function updateBudgetSummary() {
+    document.getElementById("totalBudget").innerText = (totalBudget + totalIncome).toFixed(2);
+    document.getElementById("totalIncome").innerText = totalIncome.toFixed(2);
+    document.getElementById("totalExpense").innerText = totalExpense.toFixed(2);
+    document.getElementById("totalBalance").innerText = (totalIncome - totalExpense).toFixed(2);
 }
